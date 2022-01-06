5939aeccbc6f06817890280ebe6fb695ff70f2e0
abirvalarg <yurnik2004@gmail.com>
2022-01-06 22:51:21 +0300

    default panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,10 @@ pub mod rcc;
 pub mod mutex;
 pub mod interrupt;
 
+#[cfg(feature = "default_panic")]
+#[panic_handler]
+fn __panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
 static RCC: mutex::Mutex<rcc::Rcc> = unsafe { mutex::Mutex::new(rcc::Rcc::new(0x4002_3800)) };
